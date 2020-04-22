dd94cd8c7b44e50a0eacd5e79c4fa76f789d6153
zackjackson <zackary.l.jackson@gmail.com>
2020-04-21 22:36:43 -0700

    nameResolution

--- streamed-federation/mock-s3/s3server.js
+++ streamed-federation/mock-s3/s3server.js
@@ -1,97 +1,122 @@
-const S3rver = require('s3rver')
-const express = require('express')
-const AWS = require('aws-sdk')
-const path = require('path')
-const fs = require('fs-extra')
-const Module = require('module')
+const S3rver = require("s3rver");
+const express = require("express");
+const AWS = require("aws-sdk");
+const path = require("path");
+const fs = require("fs-extra");
+const Module = require("module");
 
-const app = express()
+const app = express();
 
-const buckets = [{ name: 'bucket1' }]
+const buckets = [{ name: "bucket1" }];
 
-const { port } = S3rver.defaultOptions
+const { port } = S3rver.defaultOptions;
+
+function nameResolution(resolutor) {
+  let enabled = true;
+
+  const originalResolveFilename = Module._resolveFilename;
+
+  Module._resolveFilename = function (request, _parent) {
+    const match = enabled && resolutor(request, _parent);
+    if (match) {
+      const modifiedArguments = [match].concat([].slice.call(arguments, 1)); // Passes all arguments. Even those that is not specified above.
+      return originalResolveFilename.apply(this, modifiedArguments);
+    }
+
+    return originalResolveFilename.apply(this, arguments);
+  };
+
+  return () => {
+    enabled = false;
+  };
+}
 
 function requireFromString(code, filename, opts) {
-  if (typeof filename === 'object') {
-    opts = filename
-    filename = undefined
+  if (typeof filename === "object") {
+    opts = filename;
+    filename = undefined;
   }
 
-  opts = opts || {}
-  filename = filename || ''
+  opts = opts || {};
+  filename = filename || "";
 
-  opts.appendPaths = opts.appendPaths || []
-  opts.prependPaths = opts.prependPaths || []
+  opts.appendPaths = opts.appendPaths || [];
+  opts.prependPaths = opts.prependPaths || [];
 
-  if (typeof code !== 'string') {
-    throw new Error(`code must be a string, not ${typeof code}`)
+  if (typeof code !== "string") {
+    throw new Error(`code must be a string, not ${typeof code}`);
   }
+  const paths = Module._nodeModulePaths(path.dirname(filename));
 
-  const paths = Module._nodeModulePaths(path.dirname(filename))
+  const { parent } = module;
+  nameResolution((requireName, parent) => filename);
 
-  const { parent } = module
-  const m = new Module(filename, parent)
-  m.filename = filename
-  m.paths = [].concat(opts.prependPaths).concat(paths).concat(opts.appendPaths)
-  require.cache[m.id] = m
-  m._compile(code, filename)
+  const m = new Module(filename, parent);
 
-  const { exports } = m
-  parent
-    && parent.children
-    && parent.children.splice(parent.children.indexOf(m), 1)
-  m.loaded = true // added this, dont know if i need it
+  m.filename = filename;
+  m.paths = [].concat(opts.prependPaths).concat(paths).concat(opts.appendPaths);
+  require.cache[m.id] = m;
+  console.log("33333", JSON.stringify(m.id));
+  m._compile(code, filename);
 
-  return exports
+  const { exports } = m;
+  parent &&
+    parent.children &&
+    parent.children.splice(parent.children.indexOf(m), 1);
+  m.loaded = true; // added this, dont know if i need it
+
+  return exports;
 }
 
 const server = new S3rver({
   port,
-  hostname: 'localhost',
+  hostname: "localhost",
   silent: false,
-  directory: './bucket-resources',
+  directory: "./bucket-resources",
   configureBuckets: buckets,
-})
+});
 
 const initializeS3 = async () => {
-  app.use('/basepath', server.getMiddleware())
-  await server.configureBuckets()
+  app.use("/basepath", server.getMiddleware());
+  await server.configureBuckets();
 
-  let httpServer
+  let httpServer;
   await new Promise((resolve, reject) => {
-    httpServer = app.listen(S3rver.defaultOptions.port, err => err ? reject(err) : resolve())
-  })
+    httpServer = app.listen(S3rver.defaultOptions.port, (err) =>
+      err ? reject(err) : resolve()
+    );
+  });
 
   const s3Client = new AWS.S3({
-    accessKeyId: 'S3RVER',
-    secretAccessKey: 'S3RVER',
+    accessKeyId: "S3RVER",
+    secretAccessKey: "S3RVER",
     endpoint: `http://localhost:${port}/basepath`,
     sslEnabled: false,
     s3ForcePathStyle: true,
-  })
+  });
 
-  const files = [path.join(__dirname, 'resources/index.js')]
+  const files = [path.join(__dirname, "resources/index.js")];
 
   await s3Client
     .putObject({
       Bucket: buckets[0].name,
-      Key: 'index',
+      Key: "index",
       Body: await fs.readFile(files[0]),
-      ContentType: 'application/javascript',
+      ContentType: "application/javascript",
     })
-    .promise()
+    .promise();
   const object = await s3Client
-    .getObject({ Bucket: buckets[0].name, Key: 'index' })
-    .promise()
-  const streamedModule = object.Body.toString()
-  const streamedFilePath = path.join(process.cwd(), 'federatedFile.js')
+    .getObject({ Bucket: buckets[0].name, Key: "index" })
+    .promise();
+  const streamedModule = object.Body.toString();
+  const streamedFilePath = path.join(process.cwd(), "federatedFile.js");
   const readableModule = requireFromString(streamedModule, streamedFilePath, {
-    appendPaths: ['/'],
-  })
+    appendPaths: ["/"],
+  });
 
-  console.log(readableModule)
-  console.log(require.cache[require('path').resolve('./federatedFile.js')])
-  console.log(require('./federatedFile.js'))
-}
+  console.log(readableModule);
+  console.log(require.cache[require("path").resolve("./federatedFile.js")]);
+  console.log(require("./federatedFile.js"));
+};
 
-initializeS3()
+initializeS3();
